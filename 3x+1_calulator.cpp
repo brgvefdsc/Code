8442423e003080d9deaf8b47117a9433cc6f9edf
ProgamingGenesis <87057272+ProgamingGenesis@users.noreply.github.com>
2021-08-15 18:27:29 -0700

    Create 3x+1_calulator.cpp
    Here's a video explaining this math problem this is a calculator for it https://www.youtube.com/watch?v=094y1Z2wpJg&t=142s .

---
+++ 3x+1_calulator.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+void loopindc(){
+    printf("Interger has fallen into 1,2,4 loop");
+}
+
+void odd_finder (int n){
+    if(n % 2 == 0){
+        n = n/2;
+        cout << n << " ";
+        Sleep(50);
+        odd_finder(n);
+
+    }
+    else{
+        n = n*3 + 1;
+        Sleep(50);
+        cout << n << " ";
+        odd_finder(n);
+
+        
+    }
+}
+int main() {
+    int n;
+    cout << "\nPlease Press 'control-c' to stop code once loop has started (Doing this will close this window) \n \n";
+    cout << "Enter an integer: ";
+    cin >> n;
+
+    odd_finder(n);
+
+
+
+}
